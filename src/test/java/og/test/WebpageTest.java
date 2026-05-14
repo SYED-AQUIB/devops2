1399f03e6e56e160d8cd488cedea131b8a484f08
SYED-AQUIB <4bd23cd047@bietdvg.edu>
2026-05-14 22:01:55 +0530

    Third commit

---
+++ src/test/java/og/test/WebpageTest.java
@@ -0,0 +1,31 @@
+package og.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+public class WebpageTest {
+ private static WebDriver driver;
+@BeforeTest
+public void openBrowser() throws InterruptedException {
+ driver = new ChromeDriver();
+ driver.manage().window().maximize();
+ Thread.sleep(2000);
+ driver.get("https://syed-aquib.github.io/devops2/");
+}
+@Test
+public void titleValidationTest(){
+ String actualTitle = driver.getTitle();
+ System.out.println(actualTitle);
+ String expectedTitle = "My simple Website";
+ Assert.assertEquals (actualTitle, expectedTitle);
+ assertTrue(true, "Title contains 'Design'");
+}
+@AfterTest
+ public void closeBrowser() throws InterruptedException {
+ Thread.sleep(1000);
+ driver.quit();
+}
+}
